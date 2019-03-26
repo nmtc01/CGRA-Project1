9644b2ba11a52954fc2a29231db866ef68f1ad6a
Pastilhas <up201704982@fe.up.pt>
2019-03-26 15:09:22 +0000

    Auto stash before merge of "origin" and "origin/origin"

--- MyScene.js
+++ MyScene.js
@@ -10,6 +10,7 @@ class MyScene extends CGFscene {
         super.init(application);
         this.initCameras();
         this.initLights();
+        this.initMaterials();
 
         //Background color
         this.gl.clearColor(0.0, 0.0, 0.0, 1.0);
@@ -21,23 +22,122 @@ class MyScene extends CGFscene {
 
         //Initialize scene objects
         this.axis = new CGFaxis(this);
+        this.pyramid = new MyPyramid(this, 3, 1);
 
-        //Objects connected to MyInterface
+        //Other variables connected to MyInterface
+        this.selectedMaterial = 0;
+        this.displayAxis = false;
+        this.displayNormals = false;
+        this.objectComplexity = 0.5;
+        this.scaleFactor = 1.0;
+		this.intensity = 0.3;
     }
     initLights() {
-        this.lights[0].setPosition(15, 2, 5, 1);
+        this.setGlobalAmbientLight(1, 1, 1, 1);
+
+        this.lights[0].setPosition(2.0, 2.0, -1.0, 1.0);
         this.lights[0].setDiffuse(1.0, 1.0, 1.0, 1.0);
-        this.lights[0].enable();
+        this.lights[0].setSpecular(1.0, 1.0, 1.0, 1.0);
+        this.lights[0].disable();
+        this.lights[0].setVisible(true);
         this.lights[0].update();
+
+        this.lights[1].setPosition(0.0, -1.0, 2.0, 1.0);
+        this.lights[1].setDiffuse(1.0, 1.0, 1.0, 1.0);
+        this.lights[1].setSpecular(1.0, 1.0, 0.0, 1.0);
+        this.lights[1].disable();
+        this.lights[1].setVisible(true);
+        this.lights[1].update();
     }
     initCameras() {
-        this.camera = new CGFcamera(0.4, 0.1, 500, vec3.fromValues(15, 15, 15), vec3.fromValues(0, 0, 0));
+        this.camera = new CGFcamera(0.4, 0.1, 500, vec3.fromValues(10, 10, 10), vec3.fromValues(0, 0, 0));
     }
-    setDefaultAppearance() {
-        this.setAmbient(0.2, 0.4, 0.8, 1.0);
-        this.setDiffuse(0.2, 0.4, 0.8, 1.0);
-        this.setSpecular(0.2, 0.4, 0.8, 1.0);
-        this.setShininess(10.0);
+
+    hexToRgbA(hex)
+    {
+        var ret;
+        //either we receive a html/css color or a RGB vector
+        if(/^#([A-Fa-f0-9]{3}){1,2}$/.test(hex)){
+            ret=[
+                parseInt(hex.substring(1,3),16).toPrecision()/255.0,
+                parseInt(hex.substring(3,5),16).toPrecision()/255.0,
+                parseInt(hex.substring(5,7),16).toPrecision()/255.0,
+                1.0
+            ];
+        }
+        else
+            ret=[
+                hex[0].toPrecision()/255.0,
+                hex[1].toPrecision()/255.0,
+                hex[2].toPrecision()/255.0,
+                1.0
+            ];
+        return ret;
+    }
+
+    updateCustomMaterial() {
+        var rgba;
+
+        this.customMaterial.setAmbient(...this.hexToRgbA(this.customMaterialValues['Ambient']));
+        this.customMaterial.setDiffuse(...this.hexToRgbA(this.customMaterialValues['Diffuse']));
+        this.customMaterial.setSpecular(...this.hexToRgbA(this.customMaterialValues['Specular']));
+
+        this.customMaterial.setShininess(this.customMaterialValues['Shininess']);
+
+    };
+
+    updateObjectComplexity(){
+		this.pyramid.updateBuffers(this.objectComplexity);
+    }
+
+
+    initMaterials() {
+        // Red Ambient (no diffuse, no specular)
+        this.material1 = new CGFappearance(this);
+        this.material1.setAmbient(1, 0, 0, 1.0);
+        this.material1.setDiffuse(0, 0, 0, 1.0);
+        this.material1.setSpecular(0, 0, 0, 1.0);
+        this.material1.setShininess(10.0);
+
+        // Red Diffuse (no ambient, no specular)
+        this.material2 = new CGFappearance(this);
+        this.material2.setAmbient(0.0, 0.0, 0.0, 1.0);
+        this.material2.setDiffuse(1, 0, 0, 1.0);
+        this.material2.setSpecular(0, 0, 0, 1.0);
+        this.material2.setShininess(10.0);
+
+        // Red Specular (no ambient, no diffuse)
+        this.material3 = new CGFappearance(this);
+        this.material3.setAmbient(0, 0, 0, 1.0);
+        this.material3.setDiffuse(0, 0, 0, 1.0);
+        this.material3.setSpecular(1, 0, 0, 1.0);
+        this.material3.setShininess(10.0);
+
+        // Custom material (can be changed in the interface)
+        // initially midrange values on ambient, diffuse and specular, on R, G and B respectively
+
+        this.customMaterialValues = {
+            'Ambient': '#0000ff',
+            'Diffuse': '#ff0000',
+            'Specular': '#000000',
+            'Shininess': 10
+        }
+        this.customMaterial = new CGFappearance(this);
+
+        this.updateCustomMaterial();
+		
+		
+        // Wood (low specular)
+        this.material4 = new CGFappearance(this);
+        this.material4.setAmbient(0.8, 0.6, 0.4, 1.0);
+        this.material4.setDiffuse(0.8, 0.6, 0.4, 1.0);
+        this.material4.setSpecular(0.8, 0.6, 0.4, 0.2);
+        this.material4.setShininess(1.0);
+
+        this.materials = [this.material1, this.material2, this.material3, this.customMaterial, this.material4];
+
+        // Labels and ID's for object selection on MyInterface
+        this.materialIDs = {'Red Ambient': 0, 'Red Diffuse': 1, 'Red Specular': 2, 'Custom': 3, 'Wood': 4 };
     }
     display() {
         // ---- BEGIN Background, camera and axis setup
@@ -49,16 +149,30 @@ class MyScene extends CGFscene {
         this.loadIdentity();
         // Apply transformations corresponding to the camera position relative to the origin
         this.applyViewMatrix();
+        
+        this.lights[0].update();
+        this.lights[1].update();
 
         // Draw axis
-        this.axis.display();
-
-        //Apply default appearance
-        this.setDefaultAppearance();
+        if (this.displayAxis)
+            this.axis.display();
 
         // ---- BEGIN Primitive drawing section
 
+        this.materials[this.selectedMaterial].apply();
 
+        this.pushMatrix();
+        this.scale(this.scaleFactor,this.scaleFactor,this.scaleFactor);
+        
+        if (this.displayNormals)
+            this.pyramid.enableNormalViz();
+        else
+            this.pyramid.disableNormalViz();
+        
+        this.setGlobalAmbientLight(this.intensity, this.intensity, this.intensity, 1);
+		
+        this.pyramid.display();
+        this.popMatrix();
         // ---- END Primitive drawing section
     }
 }
