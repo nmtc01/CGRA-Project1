c620078be0d7b4c29982865b65433ad911d8cc1c
nmtc01 <39002030+nmtc01@users.noreply.github.com>
2019-03-26 15:08:05 +0000

    Added MyPrism

---
+++ MyPrism.js
@@ -0,0 +1,15 @@
+/**
+ * MyPrism
+ * @constructor
+ * @param scene - Reference to MyScene object
+ */
+class MyDiamond extends CGFobject {
+	constructor(scene) {
+		super(scene);
+		this.initBuffers();
+	}
+
+	initBuffers() {
+	    
+	}
+}
