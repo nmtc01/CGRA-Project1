2e0226f844e96998fe4da35ddedae4100b2afc19
Pastilhas <joao.campos1221@gmail.com>
2019-03-31 18:49:02 +0100

    Finished VoxelHill

--- MyVoxelHill.js
+++ MyVoxelHill.js
@@ -15,9 +15,35 @@ class MyVoxelHill extends CGFobject {
 	display(){
         this.scene.pushMatrix();
 
-        for(var i = height; i > 0; i--){
-
-
+        for(var i = 0; i < this.height; i++){
+             this.scene.pushMatrix();
+             this.scene.translate(-i,this.height-i,-i);         //BACK
+             for(var j = 0; j < 2*i; j++){
+                     this.block.display();
+                     this.scene.translate(1,0,0);
+             }
+             this.scene.popMatrix();
+             this.scene.pushMatrix();
+             this.scene.translate(i,this.height-i,-i);
+             for(var j = 0; j <= 2*i; j++){                     //RIGHT
+                     this.block.display();
+                     this.scene.translate(0,0,1);
+             }
+             this.scene.popMatrix();
+             this.scene.pushMatrix();
+             this.scene.translate(i,this.height-i,i);
+             for(var j = 0; j <= 2*i; j++){                     //FRONT
+                     this.block.display();
+                     this.scene.translate(-1,0,0);
+             }
+             this.scene.popMatrix();
+             this.scene.pushMatrix();
+             this.scene.translate(-i,this.height-i,i);
+             for(var j = 0; j <= 2*i; j++){                     //LEFT
+                     this.block.display();
+                     this.scene.translate(0,0,-1);
+             }
+             this.scene.popMatrix();
         }
 
         this.scene.popMatrix();
